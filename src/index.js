c52a152ba127ce4a877064863e1b53b314b0a5ee
bailey-redbrick <bailey@redbrick.space>
2022-12-31 01:30:34 +0900

    Feat: Add map object copy

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ export const copyObj = (origin) => {
 
 	return res;
 };
+
+export const copyMap = (origin) => {
+	if (origin instanceof Map) {
+		const res = new Map();
+		for (let [key, value] of origin) {
+			if (typeof origin.get(key) === 'object') {
+				const objValue = copyObj(value);
+				res.set(key, objValue);
+			} else {
+				res.set(key, origin.get(key));
+			}
+		}
+		return res;
+	}
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,19 @@
-const { copyObj } = require('.');
+const { copyObj, copyMap } = require('.');
 
-describe('test copy obj', () => {
+describe('test copy object', () => {
 	const obj = {
 		a: 1,
 	};
 
-	test('copied obj has the same property key and value', () => {
+	test('copied object has the same property key and value', () => {
 		expect(copyObj(obj)).toEqual(obj);
 	});
 
-	test('copied obj has a different reference from obj', () => {
+	test('copied object has a different reference from obj', () => {
 		expect(copyObj(obj)).not.toBe(obj);
 	});
 
-	test('it calls function recursively if the value is an object', () => {
+	test('recursive calls if the value is an object', () => {
 		const obj = {
 			b: {
 				c: 2,
@@ -22,5 +22,25 @@ describe('test copy obj', () => {
 
 		expect(copyObj(obj)).toEqual(obj);
 		expect(copyObj(obj)).not.toBe(obj);
+	});
+});
+
+describe('test copy map object', () => {
+	const mapObj = new Map();
+	mapObj.set('a', 1);
+
+	test('copy primitive property in map object', () => {
+		expect(copyMap(mapObj)).not.toBe(mapObj);
+		expect(copyMap(mapObj)).toEqual(mapObj);
+	});
+
+	test('copy object in map object', () => {
+		var mapObj = new Map([
+			['A', { value: 'valueA' }],
+			['B', { value: 'valueB' }],
+			['C', { value: 'valueC' }],
+		]);
+		expect(copyMap(mapObj)).not.toBe(mapObj);
+		expect(copyMap(mapObj)).toEqual(mapObj);
 	});
 });
