37c92c41a52e894f5293d29ddd87835f2f2556a6
bailey-redbrick <bailey@redbrick.space>
2023-01-01 14:30:15 +0900

    Refactor test
    - edit test description
    - add error test
    - add error handling to deepCopy()

--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,16 @@
 const { copyObj, copyMap, copySet, deepCopy } = require('.');
 
-describe('test copy object', () => {
-	const obj = {
-		a: 1,
-	};
+describe('test copying object', () => {
+	test('copy primitive value in object', () => {
+		const obj = {
+			a: 1,
+		};
 
-	test('copied object has the same property key and value', () => {
+		expect(copyObj(obj)).not.toBe(obj);
 		expect(copyObj(obj)).toEqual(obj);
 	});
 
-	test('copied object has a different reference from obj', () => {
-		expect(copyObj(obj)).not.toBe(obj);
-	});
-
-	test('recursive calls if the value is an object', () => {
+	test('copy object value in object', () => {
 		const obj = {
 			b: {
 				c: 2,
@@ -25,37 +22,38 @@ describe('test copy object', () => {
 	});
 });
 
-describe('test copy map object', () => {
-	const mapObj = new Map();
-	mapObj.set('a', 1);
+describe('test copying map object', () => {
+	test('copy primitive value in map object', () => {
+		const mapObj = new Map();
+		mapObj.set('a', 1);
 
-	test('copy primitive property in map object', () => {
 		expect(copyMap(mapObj)).not.toBe(mapObj);
 		expect(copyMap(mapObj)).toEqual(mapObj);
 	});
 
-	test('copy object in map object', () => {
+	test('copy object value in map object', () => {
 		var mapObj = new Map([
 			['A', { value: 'valueA' }],
 			['B', { value: 'valueB' }],
 			['C', { value: 'valueC' }],
 		]);
+
 		expect(copyMap(mapObj)).not.toBe(mapObj);
 		expect(copyMap(mapObj)).toEqual(mapObj);
 	});
 });
 
-describe('test copy set object', () => {
+describe('test copying set object', () => {
 	const setObj = new Set();
-	setObj.add(1);
-	setObj.add(5);
 
-	test('copy primitive property in set object', () => {
+	test('copy primitive value in set object', () => {
+		setObj.add(1);
+		setObj.add(5);
 		expect(copySet(setObj)).not.toBe(setObj);
 		expect(copySet(setObj)).toEqual(setObj);
 	});
 
-	test('copy primitive property in set object', () => {
+	test('copy object value in set object', () => {
 		setObj.add({ a: 1, b: 2 });
 
 		expect(copySet(setObj)).not.toBe(setObj);
@@ -64,7 +62,14 @@ describe('test copy set object', () => {
 });
 
 describe('test deepCopy()', () => {
-	test('deep copy object depends on object type', () => {
+	test('make a deep copy of an object depends on type', () => {
+		const obj = {
+			a: 1,
+			b: {
+				c: 2,
+			},
+		};
+
 		const setObj = new Set();
 		setObj.add(1);
 		setObj.add(5);
@@ -77,16 +82,17 @@ describe('test deepCopy()', () => {
 			['D', 'valueD'],
 		]);
 
-		const obj = {
-			a: 1,
-			b: {
-				c: 2,
-			},
-		};
-
+		expect(deepCopy(obj)).toEqual(obj);
 		expect(deepCopy(setObj)).toEqual(setObj);
 		expect(deepCopy(mapObj)).toEqual(mapObj);
-		expect(deepCopy(obj)).toEqual(obj);
+	});
+
+	test('throw an error if the parameter is primitive', () => {
+		expect(() => deepCopy(4)).toThrow('unsupported type');
+		expect(() => deepCopy('sdf')).toThrow('unsupported type');
+	});
+
+	test('throw an error if the parameter type is array', () => {
+		expect(() => deepCopy([1, 2, 3])).toThrow('unsupported type');
 	});
 });
-// TODO: test object value reference
